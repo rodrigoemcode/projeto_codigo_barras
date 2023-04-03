40e03fd46154c2291ea4c0f7d12ed8e05226ba6a
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2023-04-03 11:03:31 -0300

    Correção dos arquivos

--- consulta_produtos/webapp/controller/Main.controller.js
+++ consulta_produtos/webapp/controller/Main.controller.js
@@ -1,23 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
 
-        return Controller.extend("consultaprodutos.controller.Main", {
-            onInit: function () {
-                //alert("Meu programa está no ar!!!");
-            },
-            onPressBuscar: function() {
-                let input;
-                input = this.byId("inpBusca");
-                let valor = input.getValue();
-                alert(valor);
+	return Controller.extend("consultaprodutos.controller.Main", {
 
-                let 
-            }
-        });
-    });
+		onInit: function () {
+		},
+
+		onPressBuscar: function () {
+			var input = this.byId("inpBusca");
+			var codigoBarras = input.getValue();
+
+			// Cria uma instância do modelo
+			var oModel = new sap.ui.model.json.JSONModel();
+
+			// Define a URL do serviço
+			var sUrl = "/produto/busca_produto.php?code=" + codigoBarras;
+
+			// Define a propriedade 'model' da View para o modelo criado
+			this.getView().setModel(oModel);
+
+			// Define a propriedade 'source' do ObjectHeader para o modelo criado
+			var oObjectHeader = this.byId("objHeader");
+			oObjectHeader.setModel(oModel);
+
+			// Faz a requisição para o serviço
+			jQuery.ajax({
+				type: "GET",
+				contentType: "application/json",
+				url: sUrl,
+				dataType: "json",
+				async: false,
+				success: function (data, textStatus, jqXHR) {
+					oModel.setData(data);
+				},
+				error: function (jqXHR, textStatus, errorThrown) {
+					MessageToast.show("Erro na requisição");
+				}
+			});
+		}
+	});
+});
